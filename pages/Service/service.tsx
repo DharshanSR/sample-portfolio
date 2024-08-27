8af892aedd732fcd957b831c1626689d274d7163
DharshanSR <dharshanravindran8@gmail.com>
2024-08-27 23:59:12 +0530

    some final touch

--- pages/Service/service.tsx
+++ pages/Service/service.tsx
@@ -7,6 +7,7 @@ import webDevelopment from "@/animation/web_development.json";
 import mobileAppDevelopment from "@/animation/mobile_app_development.json";
 import machineLearning from "@/animation/ai_machine_learning.json";
 import contentWriting from "@/animation/content_writing.json";
+import uiDesign from "@/animation/female_Web_Designer_working_on_web_design.json"
 import arrowAnimation from "@/animation/arrow.json";
 import { motion } from 'framer-motion';
 import useProjectInView from '@/hooks/useProjectInView';
@@ -15,28 +16,34 @@ const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
 
 const services = [
   {
-    title: "Web Development",
-    description: "Designing and developing responsive, high-performance websites utilizing advanced technologies like Next.js, TypeScript, and Tailwind CSS. Focused on creating seamless user experiences and robust functionality tailored to modern web standards.",
+    title: "Full Stack Development",
+    description: "Developing responsive, high-performance web applications with React, Next.js, TypeScript, Tailwind CSS, Node.js, and Express. Utilizing MongoDB, PostgreSQL, Docker, and CI/CD for robust, scalable solutions. Focused on seamless user experiences and scalable backend functionality.",
     animationData: webDevelopment,
     link: "/web-development",
   },
   {
     title: "Mobile App Development",
-    description: "Crafting sophisticated and scalable mobile applications for both iOS and Android platforms. Emphasizing user-centric design, intuitive interfaces, and optimized performance to deliver exceptional mobile experiences and meet diverse user needs.",
+    description: "Building scalable mobile apps for iOS and Android using React Native and Flutter. Emphasizing user-centric design and performance with Redux, Firebase, and native APIs for optimized, intuitive experiences. Focused on seamless user experience",
     animationData: mobileAppDevelopment,
     link: "/mobile-app-development",
   },
   {
     title: "Machine Learning & AI",
-    description: "Building advanced machine learning models and intelligent systems that leverage extensive data analysis and predictive modeling. Aimed at enhancing decision-making, automating complex processes, and driving innovation through data-driven insights.",
+    description: "Developing advanced machine learning models and intelligent systems that utilize comprehensive data analysis and predictive modeling. Focused on enhancing decision-making, automating complex processes, and driving innovation through data-driven insights.",
     animationData: machineLearning,
     link: "/machine-learning",
   },
   {
     title: "Blog & Article Writing",
-    description: "Creating insightful and engaging blog content on Medium and other platforms. Sharing expertise and perspectives on technology trends, industry advancements, and best practices to inform and inspire a diverse audience of readers and professionals.",
+    description: "Crafting insightful and engaging blog content for Medium, Stack Overflow, and other platforms. Sharing expertise on technology trends, industry advancements, and best practices to inform and inspire a diverse audience of readers and professionals.",
     animationData: contentWriting,
     link: "/blog-writing",
+  },
+  {
+    title: "UI & UX Design",
+    description: "Delivering exceptional UI/UX design services by combining user research, interactive prototyping, and visual design to create intuitive and engaging interfaces. Focused on enhancing user experiences through thoughtful design, usability testing, and accessibility best practices.",
+    animationData: uiDesign,
+    link: "/ui-ux-design",
   },
 ];
 
@@ -101,7 +108,7 @@ const Expertise: React.FC = () => {
               transition={{ duration: 0.5, delay: index * 0.2 }}
               className="w-full md:w-1/2 lg:w-1/3 p-4"
             >
-              <div className="relative bg-gray-800 rounded-lg shadow-lg p-6 h-full transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden cursor-pointer group">
+              <div className="relative bg-gray-800 hover:bg-gradient-to-r from-gray-800 to-gray-700 rounded-lg shadow-lg p-6 h-full transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden cursor-pointer group">
                 {isClient && service.animationData && (
                   <div className="inset-0 flex items-center justify-center overflow-hidden cursor-pointer">
                     <Lottie
@@ -137,20 +144,20 @@ const Expertise: React.FC = () => {
       </div>
 
       {/* Bottom SVG Divider */}
-      <div className="w-full overflow-hidden">
+      {/* <div className="w-full overflow-hidden">
         <svg
-          className="w-full h-16"
-          viewBox="0 0 1200 120"
+          className="w-full h-32"
+          viewBox="0 0 1200 320"
           xmlns="http://www.w3.org/2000/svg"
           preserveAspectRatio="none"
         >
           <path
-            d="M0,50 C350,100 650,100 800,50 C950,0 1200,0 1200,0 L1200,120 L0,120 Z"
+            d="M0,160 C150,260 350,60 600,120 C850,180 1000,20 1200,100 L1200,320 L0,320 Z"
             className="fill-current text-gray-700"
           ></path>
         </svg>
-      </div>
-    </section>
+      </div> */}
+</section>
   );
 };
 
