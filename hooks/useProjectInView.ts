c28746e27fd23d46efe5a168a93368f02fe6675c
DharshanSR <dharshanravindran8@gmail.com>
2024-09-03 23:33:35 +0530

    change some animation

--- hooks/useProjectInView.ts
+++ hooks/useProjectInView.ts
@@ -1,6 +1,6 @@
 import { useInView } from 'react-intersection-observer';
 
-const useProjectInView = (threshold = 0.2) => {
+const useProjectInView = (threshold = 0.1) => {
   const { ref, inView } = useInView({
     threshold,
     triggerOnce: false, // Ensure animations trigger every time the element comes into view
